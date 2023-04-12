ae4400930db51a350fa54f33c7231addb8378032
Remya Raj <remyaraj1289@gmail.com>
2023-04-12 00:15:06 -0400

    Adding code

---
+++ config/settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // mongoDBUrl : 'mongodb://127.0.0.1:27017/Asn4-richu-db',
+    mongoDBUrl : 'mongodb+srv://remyaraj1289:pU7XZIW85bIrAuDg@cluster0.nmzmdy0.mongodb.net/Week13?retryWrites=true&w=majority',
+    secret: 'eggwhitesonly'
+}
